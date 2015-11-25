8954a3e9db10d42dabdec18482d784cc0bc6b667
Daker Pinheiro <dfp@cin.ufpe.br>
2015-11-25 01:35:22 -0300

    Added color

---
+++ src/color.h
@@ -0,0 +1,10 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include <glm/glm.hpp>
+
+namespace PathTrace {
+typedef glm::vec3 color;
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <glm/glm.hpp>
 
 #include "scene.h"
+#include "color.h"
 
 using namespace glm;
 
@@ -10,6 +11,10 @@ int main(int argc, char const *argv[])
 	vec3 x(1, 0, 0);
 	vec3 y(0, 1, 0);
 	vec3 z(0, 0, 1);
+
+	PathTrace::color r(1, 0, 0);
+	PathTrace::color g(0, 1, 0);
+	PathTrace::color b(0, 0, 1);
 
 	PathTrace::Scene s;
 
