aa95d7140a8fbdd46d6f408e545d632d31bce981
Daker Pinheiro <dfp@cin.ufpe.br>
2015-11-30 16:27:24 -0300

    [material] moved implementation to .cpp

--- src/material.h
+++ src/material.h
@@ -7,24 +7,16 @@ using namespace glm;
 
 namespace PathTrace {
 class Material {
-
 public:
-	Material();
-	Material(vec3 color, float ka, float kd, float ks, float kt, int n)
-        : color(color)
-        , ka(ka)
-        , kd(kd)
-        , ks(ks)
-        , kt(kt)
-        , n(n)
-    {
-    }
+    Material();
+    Material(vec3 color, float ka, float kd, float ks, float kt, int n);
 
 private:
-	vec3 color;
-	float ka, kd, ks, kt;
-	int n;
+    vec3 color;
+    float ka, kd, ks, kt;
+    int n;
 };
+
 }
 
 #endif
