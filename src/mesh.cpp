20387a32655dab520964e2882e7130316fd9aa00
Marcel <marcelsan_@hotmail.com>
2015-11-30 21:28:48 -0300

    Add mesh class

---
+++ src/mesh.cpp
@@ -0,0 +1,56 @@
+#include "mesh.h"
+
+namespace PathTrace {
+
+Mesh::Mesh()
+{
+
+}
+
+Mesh::Mesh(const Material& m) : Object(m)
+{
+
+};
+
+Mesh::~Mesh()
+{
+
+}
+
+Intersection Quadric::intersect(const Ray& ray) const 
+{
+    // ToDO: implement intersection with mesh
+
+    return {};
+}
+
+void Mesh::addVertex(vec3 vertex)
+{
+    vertices.push_back(vertex.x);
+    vertices.push_back(vertex.y);
+    vertices.push_back(vertex.z);
+}
+
+void Mesh::addNormals(vec3 normal)
+{
+    normals.push_back(normal.x);
+    normals.push_back(normal.y);
+    normals.push_back(normal.z);
+}
+
+void Mesh::addTriangleIndices(int a, int b, int c)
+{
+    indices.push_back(a);
+    indices.push_back(b);
+    indices.push_back(c);
+}
+
+void Mesh::resetObject()
+{
+    this->vertices.clear();
+    this->indices.clear();
+    this->normalIndices.clear();
+    this->normals.clear();
+}
+
+}
---
+++ src/mesh.h
@@ -0,0 +1,32 @@
+#ifndef QUADRIC_H
+#define QUADRIC_H
+
+#include "object.h"
+
+using namespace glm;
+
+namespace PathTrace {
+
+class Mesh : public Object
+{
+private:
+    std::vector<float> vertices;
+    std::vector<float> normals; 
+    std::vector<unsigned int> indices;
+    std::vector<unsigned int> normalIndices;
+
+public:
+    Mesh();
+    Mesh(const Material& m);
+    virtual ~Mesh();
+    Intersection intersect(const Ray& ray) const;
+    void addVertex(vec3 vertex);
+    void addNormals(vec3 normal);
+    void addTriangleIndices(int a, int b, int c);
+    void resetObject();
+
+};
+
+}
+
+#endif
