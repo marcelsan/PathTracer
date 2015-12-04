ec514814b44fc7b1c0452edcf3f5197018f6ceb0
Daker Pinheiro <dfp@cin.ufpe.br>
2015-12-04 17:48:00 -0300

    [main] tabs -> space

--- src/main.cpp
+++ src/main.cpp
@@ -17,27 +17,27 @@ using namespace PathTrace;
 
 int main(int argc, char const *argv[])
 {
-	vec3 x(1, 0, 0);
-	vec3 y(0, 1, 0);
-	vec3 z(0, 0, 1);
+    vec3 x(1, 0, 0);
+    vec3 y(0, 1, 0);
+    vec3 z(0, 0, 1);
 
-	color r(1, 0, 0);
-	color g(0, 1, 0);
-	color b(0, 0, 1);
+    color r(1, 0, 0);
+    color g(0, 1, 0);
+    color b(0, 0, 1);
 
-	Scene s;
-	Camera cam;
+    Scene s;
+    Camera cam;
 
-	if (argc < 2)
-		exit(-1);
+    if (argc < 2)
+        exit(-1);
 
-	std::string path(argv[1]);
-	FileSystemUtil::readSDLFile(path, cam, s);
+    std::string path(argv[1]);
+    FileSystemUtil::readSDLFile(path, cam, s);
 
-	Material mat(vec3(1,0,0), 10.0, 1.0, 1.0, 1.0, 1);
-	s.add(std::unique_ptr<Object>(new Quadric(10,2,3,4,5,6,7,8,9,0, mat)));
+    Material mat(vec3(1,0,0), 10.0, 1.0, 1.0, 1.0, 1);
+    s.add(std::unique_ptr<Object>(new Quadric(10,2,3,4,5,6,7,8,9,0, mat)));
 
-	ImageBuffer<color> ib(100, 100);
+    ImageBuffer<color> ib(100, 100);
 
-	return 0;
+    return 0;
 }
