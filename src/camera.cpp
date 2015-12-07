d751b187a43264d959d84041044bdf0f034ed82f
Daker Pinheiro <dfp@cin.ufpe.br>
2015-12-07 15:38:56 -0300

    [camera] refactored ray return

--- src/camera.cpp
+++ src/camera.cpp
@@ -11,10 +11,7 @@ Ray Camera::ray(float i, float j) const
     vec2 clip = clipMax - clipMin;
     vec3 dest = vec3(clipMin.x, clipMin.y, 0) + vec3(clip.x * j, clip.y * i, 0);
 
-    Ray r;
-    r.o = eye;
-    r.d = normalize(dest - eye);
-    return r;
+    return {eye, dest - eye};
 }
 
 void Camera::setEye(const vec3& e)
