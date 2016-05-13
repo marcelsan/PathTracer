a27d9519159b0c85d4f2b0ef1c16563800d0d0a0
Daker Pinheiro <dakerfp@gmail.com>
2016-05-13 08:02:03 -0300

    add utility to show rgb image

---
+++ utils/showimage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.load(sys.argv[1])
+img = np.average(a[:,:,:,:3], axis=(0,))
+
+plt.imshow(img)
+plt.show()
