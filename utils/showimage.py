704598ee2683735935425ee479b685b91d475d66
Daker Pinheiro <dakerfp@gmail.com>
2016-05-13 08:09:59 -0300

    showimage: refactor: use value instead of tuple in axis

--- utils/showimage.py
+++ utils/showimage.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a = np.load(sys.argv[1])
-img = np.average(a[:,:,:,:3], axis=(0,))
+img = np.average(a[:,:,:,:3], axis=0)
 
 plt.imshow(img)
 plt.show()
