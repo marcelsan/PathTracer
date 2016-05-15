797bdd5c32076198136d534ef44fe2985cedb44f
Daker Pinheiro <dakerfp@gmail.com>
2016-05-15 20:32:15 -0300

    use imsave instead of imshow + savefig

--- utils/showimage.py
+++ utils/showimage.py
@@ -11,55 +11,44 @@ axes.yaxis.set_visible(False)
 
 # Color
 img = np.average(a[:n,:,:,:3], axis=0)
-plt.imshow(np.clip(img, 0, 1))
-plt.savefig('img/color.png')
+plt.imsave('img/color.png', np.clip(img, 0, 1))
 
 # Normal
 img = np.average(a[:n,:,:,3:6], axis=0)
-plt.imshow((np.clip(img, -1, 1) + 1)/ 2)
-plt.savefig('img/normal.png')
+plt.imsave('img/normal.png', (np.clip(img, -1, 1) + 1)/ 2)
 
 # Distance
 img = np.average(-np.clip(a[:n,:,:,6], 0, 50), axis=0)
-plt.imshow(img, cmap='Greys_r')
-plt.savefig('img/dist.png')
+plt.imsave('img/dist.png', img, cmap='Greys_r')
 
 # Sec Distance
 img = np.average(-np.clip(a[:n,:,:,7], 0, 50), axis=0)
-plt.imshow(img, cmap='Greys_r')
-plt.savefig('img/dist2.png')
+plt.imsave('img/dist2.png', img, cmap='Greys_r')
 
 # Mat rgb
 img = np.average(a[:n,:,:,8:11], axis=0)
-plt.imshow(img)
-plt.savefig('img/matcolor.png')
+plt.imsave('img/matcolor.png', img)
 
 # Mat ka
 img = np.average(a[:n,:,:,11], axis=0)
-plt.imshow(img)
-plt.savefig('img/matka.png')
+plt.imsave('img/matka.png', img, cmap='Greys_r')
 
 # Mat kd
 img = np.average(a[:n,:,:,12], axis=0)
-plt.imshow(img, cmap='Greys_r')
-plt.savefig('img/matkd.png')
+plt.imsave('img/matkd.png', img, cmap='Greys_r')
 
 # Mat kt
 img = np.average(a[:n,:,:,13], axis=0)
-plt.imshow(img, cmap='Greys_r')
-plt.savefig('img/matkt.png')
+plt.imsave('img/matkt.png', img, cmap='Greys_r')
 
 # Mat ks
 img = np.average(a[:n,:,:,14], axis=0)
-plt.imshow(img, cmap='Greys_r')
-plt.savefig('img/matks.png')
+plt.imsave('img/matks.png', img, cmap='Greys_r')
 
 # Mat n
 img = np.average(np.clip(a[:n,:,:,15], 0, 50), axis=0)
-plt.imshow(img, cmap='Greys_r')
-plt.savefig('img/matn.png')
+plt.imsave('img/matn.png', img, cmap='Greys_r')
 
 # Mat ir
 img = np.average(np.clip(a[:n,:,:,16], 0, 50), axis=0)
-plt.imshow(img, cmap='Greys_r')
-plt.savefig('img/matir.png')
+plt.imsave('img/matir.png', img, cmap='Greys_r')
