0b3df717d7d8e918c7a5c8a610eb4da332b66847
Marcel <marcelsan_@hotmail.com>
2016-05-17 17:01:18 -0300

    Added functions in util

--- src/util.cpp
+++ src/util.cpp
@@ -4,3 +4,47 @@
 double clamp(double x){ return x<0 ? 0 : x>1 ? 1 : x; } 
 int toInt(double x){ return int(pow(clamp(x),1/2.2)*255+.5); }
 bool fcmp(float a, float b, float epsilon) { return (fabs(a - b) < epsilon); }
+void printMatrix4(const glm::dmat4& M, const char* t)
+{
+    printf("M(%s) = ", t);
+    for(int i = 0; i < 4; ++i) {
+        printf("[%f %f %f %f]\n", M[i][0], M[i][1], M[i][2], M[i][3]);
+    }
+    printf("\n");
+}
+
+void printVec4(const glm::vec4& v, const char* title)
+{
+    printf("vec4(%s) = ", title);
+    printf("[%f %f %f %f]\n", v[0], v[1], v[2], v[3]);
+    printf("\n");
+}
+
+glm::dmat4 LookAt(const glm::vec3& pos, const glm::vec3& look, const glm::vec3& up) {
+    
+    glm::dmat4 cameraToWorld;
+    // Initialize fourth column of viewing matrix
+    cameraToWorld[0][3] = pos.x;
+    cameraToWorld[1][3] = pos.y;
+    cameraToWorld[2][3] = pos.z;
+    cameraToWorld[3][3] = 1;
+
+    glm::vec3 dir = glm::normalize(look - pos);
+    glm::vec3 left = glm::normalize(glm::cross(glm::normalize(up), dir));
+    glm::vec3 newUp = glm::cross(dir, left);
+
+    cameraToWorld[0][0] = left.x;
+    cameraToWorld[1][0] = left.y;
+    cameraToWorld[2][0] = left.z;
+    cameraToWorld[3][0] = 0.;
+    cameraToWorld[0][1] = newUp.x;
+    cameraToWorld[1][1] = newUp.y;
+    cameraToWorld[2][1] = newUp.z;
+    cameraToWorld[3][1] = 0.;
+    cameraToWorld[0][2] = dir.x;
+    cameraToWorld[1][2] = dir.y;
+    cameraToWorld[2][2] = dir.z;
+    cameraToWorld[3][2] = 0.;
+
+    return glm::inverse(cameraToWorld);
+}
--- src/util.h
+++ src/util.h
@@ -1,8 +1,15 @@
 #ifndef UTIL_H
 #define UTIL_H
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <cstdio>
 
 bool fcmp(const float a, const float b, const float epsilon = 0.000001f);
 double clamp(double x);
 int toInt(const double x);
 
+void printMatrix4(const glm::dmat4& M, const char* t);
+void printVec4(const glm::vec4& v, const char* title);
+
+glm::dmat4 LookAt(const glm::vec3 &pos, const glm::vec3 &look, const glm::vec3 &up);
 #endif
