3668970d5a6fbb4ca70cd5702d9de6dcdfe7311b
Daker Pinheiro <dakerfp@gmail.com>
2016-05-18 08:16:24 -0300

    numbering figures

--- utils/showimage.py
+++ utils/showimage.py
@@ -11,44 +11,44 @@ axes.yaxis.set_visible(False)
 
 # Color
 img = np.average(a[:n,:,:,:3], axis=0)
-plt.imsave('img/color.png', np.clip(img, 0, 1))
+plt.imsave('img/01-color.png', np.clip(img, 0, 1))
 
 # Normal
 img = np.average(a[:n,:,:,3:6], axis=0)
-plt.imsave('img/normal.png', (np.clip(img, -1, 1) + 1)/ 2)
+plt.imsave('img/02-normal.png', (np.clip(img, -1, 1) + 1)/ 2)
 
 # Distance
 img = np.average(-np.clip(a[:n,:,:,6], 0, 50), axis=0)
-plt.imsave('img/dist.png', img, cmap='Greys_r')
+plt.imsave('img/03-dist.png', img, cmap='Greys_r')
 
 # Sec Distance
 img = np.average(-np.clip(a[:n,:,:,7], 0, 50), axis=0)
-plt.imsave('img/dist2.png', img, cmap='Greys_r')
+plt.imsave('img/04-dist2.png', img, cmap='Greys_r')
 
 # Mat rgb
 img = np.average(a[:n,:,:,8:11], axis=0)
-plt.imsave('img/matcolor.png', img)
+plt.imsave('img/05-matcolor.png', img)
 
 # Mat ka
 img = np.average(a[:n,:,:,11], axis=0)
-plt.imsave('img/matka.png', img, cmap='Greys_r')
+plt.imsave('img/06-matka.png', img, cmap='Greys_r')
 
 # Mat kd
 img = np.average(a[:n,:,:,12], axis=0)
-plt.imsave('img/matkd.png', img, cmap='Greys_r')
+plt.imsave('img/07-matkd.png', img, cmap='Greys_r')
 
 # Mat kt
 img = np.average(a[:n,:,:,13], axis=0)
-plt.imsave('img/matkt.png', img, cmap='Greys_r')
+plt.imsave('img/08-matkt.png', img, cmap='Greys_r')
 
 # Mat ks
 img = np.average(a[:n,:,:,14], axis=0)
-plt.imsave('img/matks.png', img, cmap='Greys_r')
+plt.imsave('img/09-matks.png', img, cmap='Greys_r')
 
 # Mat n
 img = np.average(np.clip(a[:n,:,:,15], 0, 50), axis=0)
-plt.imsave('img/matn.png', img, cmap='Greys_r')
+plt.imsave('img/10-matn.png', img, cmap='Greys_r')
 
 # Mat ir
 img = np.average(np.clip(a[:n,:,:,16], 0, 50), axis=0)
-plt.imsave('img/matir.png', img, cmap='Greys_r')
+plt.imsave('img/11-matir.png', img, cmap='Greys_r')
