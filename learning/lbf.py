e9f0d3d5068c9ff8c3f7ccc7ce95fc5bc4bb6272
Daker Pinheiro <dakerfp@gmail.com>
2016-08-14 16:40:16 -0300

    Add new lbf based on Kalantari paper

--- learning/lbf.py
+++ learning/lbf.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-7.5/lib64/
 
 import sys
+import os
 import random
 import tensorflow as tf
 import numpy as np
@@ -14,7 +14,7 @@ def generate_groud_truth(filenames=[]):
         np.save('gt-' + fn, arr)
 
 
-def get_random_frames(fn, spp=16, windowsize=16):
+def get_random_frames(fn, spp=64, windowsize=55):
     arr = np.load(fn, mmap_mode='r')
     gt = np.load('gt-' + fn, mmap_mode='r')
     frames, samples, width, height, _ = arr.shape
@@ -34,10 +34,72 @@ def get_random_frames(fn, spp=16, windowsize=16):
         yield arr[rand_frame, sample_slice, width_slice, height_slice,:], \
             gt[rand_frame, width_slice, height_slice, :]
 
+def avg_and_std(frame):
+    avg = np.average(arr[:,:,:,:3], axis=0)
+    std = np.std(arr[:,:,:,:3], axis=0)
+    return np.append(avg, std, axis=3) 
+
+def show_frames(arr, gt):
+    import matplotlib.pyplot as plt
+    plt.subplot(1, 2, 1)
+    plt.imshow(np.average(arr[:,:,:,:3], axis=0))
+    plt.subplot(1, 2, 2)
+    plt.imshow(gt[:,:,:3])
+    plt.show()
+
+def create_neural_network(x, spp=64, windowsize=55):
+    layer1 = spp * windowsize * 2
+    layer2 = spp * windowsize
+    layer3 = spp * windowsize / 2
+    layer4 = layer1
+
+    # First layer
+    W1 = tf.Variable(tf.random_uniform([layer1, layer2]))
+    b1 = tf.Variable(tf.random_uniform([layer2]))
+    x1 = tf.sigmoid(tf.matmul(x, W1) + b1)
+
+    # Second layer
+    secLayerSize = spp * windowsize
+    W2 = tf.Variable(tf.random_uniform([layer2, layer3]))
+    b2 = tf.Variable(tf.random_uniform([layer3]))
+    x2 = tf.sigmoid(tf.matmul(x1, W2) + b2)
+
+    # Third layer
+    secLayerSize = spp * windowsize
+    W3 = tf.Variable(tf.random_uniform([layer3, layer4]))
+    b3 = tf.Variable(tf.random_uniform([layer4]))
+    # Using softmax is an addition to kalantari
+    x3 = tf.nn.softmax(tf.matmul(x2, W3) + b3)
+
+    return x3
+
+def mse(x, y):
+    return tf.reduce_mean(tf.square(x - y))
 
 if __name__ == '__main__':
-    generate_groud_truth(sys.argv[1:])
+    try:
+        for fn in sys.argv[1:]:
+            os.stat(fn)
+    except OSError:
+        generate_groud_truth(sys.argv[1:])
+
     random_frames = get_random_frames(sys.argv[1])
     arr, gt = next(random_frames)
     print(arr.shape)
     print(gt.shape)
+    # show_frames(arr, gt)
+
+    graph = tf.Graph()
+    with graph.as_default():
+        spp=64
+        windowsize=55
+        x = tf.placeholder(tf.float32, [1, spp * windowsize * 2], name='x') # 2 is for avg and std
+        y = create_neural_network(x, spp, windowsize)
+        y_ = tf.placeholder(tf.float32, [1, spp * windowsize * 2], name='y_')
+        train = tf.train.AdamOptimizer().minimize(mse(y, y_))
+
+        sess = tf.Session()
+        sess.run(tf.initialize_all_variables())
+
+        arr, gt = next(random_frames)
+        train.run(session=sess, feed_dict={'x': arr, 'y_': gt})
